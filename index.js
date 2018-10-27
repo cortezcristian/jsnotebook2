215c2720e491b03753ea952bf6dc23922b460eb0
Cristian Cortez <cortez.cristian@gmail.com>
2018-10-27 02:24:51 -0300

    Adding vm2 service

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-const fs = require('fs');
 const opener = require('opener');
 const express = require('express');
+const bodyParser = require('body-parser')
+const {VM} = require('vm2');
+const vm = new VM();
 
 const host = '127.0.0.1';
 const port = 3333;
 
 const app = express();
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
 app.use(express.static(__dirname + '/public'));
+
+app.post('/vm2', (req, res) => {
+  const result = {
+    stdout: null,
+    stderr: null,
+  };
+
+  console.log(req.body);
+
+  try {
+    result.stdout = vm.run(req.body.script);
+  } catch (e) {
+    result.stderr = e.message || e;
+  }
+
+  res.json({ res: result });
+});
 
 
 app.listen(port, host, () => {
